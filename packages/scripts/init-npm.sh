22cd77543c0797712c1898c54cc1869199a6edbf
sam <sam@freighttrust.com>
2020-08-31 06:08:42 -0700

    Update init-npm.sh

--- packages/scripts/init-npm.sh
+++ packages/scripts/init-npm.sh
@@ -6,3 +6,6 @@ find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && npm init -y
 
 exit 0
 
+# ls > dirlist
+#cat dirlist | xargs -L 1 mkdir
+#find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && npm init -y" \;
